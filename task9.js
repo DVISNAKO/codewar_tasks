61f8896280e5db5c32dd2a82e84a7e2b452274ef
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-01 15:49:17 +0300

    task 9

---
+++ task9.js
@@ -0,0 +1,7 @@
+// Write a function that removes the spaces from the string, 
+// then return the resultant string.
+
+function noSpace(x){
+    const characters = x.split(' ');
+    return characters.join('');
+  }
