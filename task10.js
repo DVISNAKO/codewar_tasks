042d780a4f1d16c90d537664a1f71eb28ee7934c
Dima <dmitrijs.visnakovs@inbox.lv>
2023-09-04 11:08:09 +0300

    task10

---
+++ task10.js
@@ -0,0 +1,12 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+function getSum(a, b)
+{
+   const res = a + b;
+  if (a === b ){
+    return a
+  }
+  return res;
+}
